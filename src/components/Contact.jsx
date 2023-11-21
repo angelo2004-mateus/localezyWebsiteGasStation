42af1558950d8aa24c63fdda626f23ccee60ea2a
Angelo Mateus <angelomateusnerysouza@gmail.com>
2023-11-21 01:40:28 -0300

    Update Email Token

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,14 +1,9 @@
 import React from 'react'
 
 const Contact = () => {
-
-  const email = 'angelomateusnerysouza@gmail.com';
-  const encodedEmail = encodeURIComponent(email)
-  const url = `https://formsubmit.co/${encodedEmail}`;
-
   return (
     <section id='contact' className='flex h-[50vh] w-full bg-[#EEEEEE] px-[40px]'>
-        <form method='POST' action={url} className='m-auto flex flex-col items-center justify-center px-[20px] gap-y-5 w-[1200px] h-[300px] rounded-lg bg-gray-300'>
+        <form method='POST' action='https://formsubmit.co/angelomateusnerysouza@gmail.com' className='m-auto flex flex-col items-center justify-center px-[20px] gap-y-5 w-[1200px] h-[300px] rounded-lg bg-gray-300'>
             <h2 className='text-[24px] text-center'>Envie Atualizações de Preços Para Nós</h2>
             <input placeholder='Digite o nome do posto' required name='nomePosto' className='w-[400px] max-sm:w-[90%] border border-black border-opacity-40 p-[10px] outline-none rounded-full bg-transparent' type="text" />
             <input placeholder='Digite o valor da combustível' required name='valor' className='w-[400px] max-sm:w-[90%] border border-black border-opacity-40 p-[10px] outline-none rounded-full bg-transparent' type="text" />
