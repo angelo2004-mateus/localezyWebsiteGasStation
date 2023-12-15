d94d6bd5ae11ab1e10fd9b7422799d63fbce6cb9
Angelo Mateus <angelomateusnerysouza@gmail.com>
2023-12-14 22:41:08 -0300

    Update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,10 +3,10 @@ import Button from './Button'
 
 const Navbar = () => {
   return (
-    <nav className='flex items-center justify-between p-[40px] w-full bg-[#EEEEEE]'>
+    <nav className='flex items-center justify-between p-[40px] w-full bg-[#EEEEEE] max-md:flex-col gap-y-10'>
         <h2 className='font-bold text-[24px] flex'>Local<span className='text-[#ED2A25]'>ezy.</span></h2>
         <img src="https://th.bing.com/th/id/OIP.hSocEZKok4SB0YeHkzRIiwHaCw?w=341&h=130&c=7&r=0&o=5&pid=1.7" alt="" />
-        <Button link={'#contact'} propLink={'self'} content={'Contato'} className={'py-[.1rem] no-underline'}/>
+        <Button link={'#contact'} propLink={'self'} content={'Contato'} className={'max-md:hidden py-[.1rem] no-underline'}/>
 
     </nav>
   )
