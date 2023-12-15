b5c1609e1e47b0f152c2252818e95c26b24ee602
Angelo Mateus <angelomateusnerysouza@gmail.com>
2023-12-14 22:35:06 -0300

    Update

--- src/components/AboutUs.jsx
+++ src/components/AboutUs.jsx
@@ -6,16 +6,11 @@ const AboutUs = () => {
         <div className='text-justify relative flex flex-col self-start max-w-[800px] font-bold text-[84px] max-md:text-[50px] tracking-tighter leading-none'>
             <span className='z-50'>Sobre Nós.</span>
             <p className='mt-[1rem] text-[16px] tracking-normal font-normal'>
-               Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima odio expedita, aspernatur nam eos officiis iusto enim alias omnis neque aliquid quo vero? Qui, neque? Consectetur, pariatur nisi? Maxime, dolorem?
-               Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima odio expedita, aspernatur nam eos officiis iusto enim alias omnis neque aliquid quo vero? Qui, neque? Consectetur, pariatur nisi? Maxime, dolorem?
-               Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima odio expedita, aspernatur nam eos officiis iusto enim alias omnis neque aliquid quo vero? Qui, neque? Consectetur, pariatur nisi? Maxime, dolorem?
+            Nós, estudantes da Fasipe Cuiabá, somos apaixonados por tecnologia e inovação. Desenvolvemos o Localezy, um guia de preços de postos de gasolina na cidade, visando facilitar a vida dos moradores. Com criatividade e comprometimento, buscamos impactar positivamente a comunidade, oferecendo uma solução prática para economizar tempo e dinheiro ao abastecer.
             </p>
 
             <p className='mt-[1rem] text-[16px] tracking-normal font-normal'>
-               Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima odio expedita, aspernatur nam eos officiis iusto enim alias omnis neque aliquid quo vero? Qui, neque? Consectetur, pariatur nisi? Maxime, dolorem?
-               Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima odio expedita, aspernatur nam eos officiis iusto enim alias omnis neque aliquid quo vero? Qui, neque? Consectetur, pariatur nisi? Maxime, dolorem?
-               Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima odio expedita, aspernatur nam eos officiis iusto enim alias omnis neque aliquid quo vero? Qui, neque? Consectetur, pariatur nisi? Maxime, dolorem?
-            </p>
+            O Localezy é o seu guia online para os preços dos postos de gasolina em Cuiabá. Oferecemos informações atualizadas e confiáveis para ajudar você a encontrar os melhores preços de combustível na cidade. Nosso objetivo é simplificar sua busca, economizando seu tempo e dinheiro, para que possa tomar decisões informadas na hora de abastecer. Junte-se a nós e descubra uma maneira mais fácil de encontrar os melhores preços de gasolina em Cuiabá.</p>
         </div>
     </section>
   )
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Button = ({ content, className, link, propLink}) => {
   return (
-    <button className={`${className} rounded text-[18px] font-normal underline text-white bg-[#ED2A25] px-[1rem] py-[.5rem]`}>
+    <button className={` rounded text-[18px] font-normal underline text-white bg-[#ED2A25] px-[1rem] py-[.5rem]`}>
        <a href={link} target={'_' + propLink}>{content}</a> 
     </button>
   )
